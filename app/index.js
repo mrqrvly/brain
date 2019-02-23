c364e0357e191405f2e5c442817123c7f8dcfaa7
Clyde Walter <cfwalter@Clydes-MacBook-Pro.local>
2019-02-23 16:51:03 -0500

    got steady, slower data stream sending commands to madmapper

--- app/index.js
+++ app/index.js
@@ -4,91 +4,91 @@ const lib = require('../lib/lib'),
       express = require('express'),
       app = express(),
       port = 12019//,
-      // Ganglion = require('openbci-ganglion'),
-      // ganglion = new Ganglion({
-      //   verbose: true,
-      //   sendCounts: true
-      // }),
+      Ganglion = require('@openbci/ganglion'),
+      ganglion = new Ganglion({
+        verbose: true,
+        sendCounts: true
+      }),
       osc = require ('osc'),
-        udpPort = new osc.UDPPort({
-          localAddress: "localhost",
-          localPort: 8081,
-          metadata: true
-        });
+      udpPort = new osc.UDPPort({
+        localAddress: "localhost",
+        localPort: 8081,
+        metadata: true
+      }),
+      utils = require('@openbci/utilities');
 
 // start;
-lib.log("starting");
-
-// routing;
-// app.get('/', (req, res) => {
-//   res.send('OBLSK Brain is awakening...');
-//   // when home route is selected, search for a Ganglion;
-//   // ganglion.searchStart();
-// });
-
-// start up the express server;
-// app.listen(port, () => {
-//   lib.log("The OBLSK brain is listening on port 12019");
-// });
-
-// monitor oscPort traffic;
-// this doesn't seem to work as of 2019.02.19;
-// udpPort.on("message", (oscMsg) => {
-//   console.log("oscPort traffic: " + oscMsg);
-// });
-
-// when a udp connection completes;
-// this one doesn't seem to work either, again as of 2019.02.19 //
-// udpPort.on("bundle", (oscBundle, timeTag, info) => {
-//   console.log("An OSC bundle just arrived for time tag", timeTag, ":", oscBundle);
-//   console.log("Remote info is: ", info);
-// });
+console.log("starting");
 
 // Ganglion event triggers;
-// ganglion.once('ganglionFound', (peripheral) => {
-//   // stop searching for BLE devices once a ganglion is found.
-//   ganglion.searchStop();
-//   // when a data sample is received from Ganglion;
-//   ganglion.on('sample', (sample) => {
-//     // lib.log(JSON.stringify(sample));
-//     let eegConverted = lib.convertEegInts(sample.channelDataCounts);
-//     console.log(eegConverted);
-//     // send message via udp(osc) to madmapper/modulate;
+ganglion.once('ganglionFound', (peripheral) => {
+  // stop searching for BLE devices once a ganglion is found.
+  ganglion.searchStop();
+  // when a data sample is received from Ganglion;
+  ganglion.on('sample', (sample) => {
+    if (sample.sampleNumber === 1) {
+      let eegConverted = lib.convertEegInts(sample.channelDataCounts);
+      console.log(eegConverted);
+      // send message via udp(osc) to madmapper/modulate;
+      eegConverted.forEach((value, index) => {
+        setTimeout(() => {
+          let quad = index + 2,
+              opacity = value,
+              quadCmd = { address: `/surfaces/Quad-${quad}/select` },
+              opacityCmd = { address: `/surfaces/Quad-${quad}/opacity`, args: [{ type: "i", value: opacity }]};
+          udpPort.send(quadCmd, "localhost", 8010);
+          console.log("sending " + JSON.stringify(quadCmd));
+          udpPort.send(opacityCmd, "localhost", 8010);
+          console.log("sending " + JSON.stringify(opacityCmd));
+        }, 250 * index);
+      });
+    }
+  });
 
-//     //
-//   });
-//   // when Ganglion is ready to offer up the fruits of the psychic organ;
-//   ganglion.once('ready', () => {
-//     // open port for communications;
-//     udpPort.open();
-//     ganglion.streamStart();
-//   });
-//   // connect Ganglion to the PC etc;
-//   ganglion.connect(peripheral);
-// });
+  // when Ganglion is ready to offer up the fruits of the psychic organ;
+  ganglion.once('ready', () => {
+    // open port for communications;
+    console.log("Ganglion ready, opening udpPort connection for osc communication");
+    udpPort.open();
+    // start Ganglion data stream //
+    ganglion.streamStart();
+    // check state of Ganglion //
+    console.log("Ganglion options at init:\n" + JSON.stringify(ganglion.options));
+  });
+  // connect Ganglion to the PC etc;
+  ganglion.connect(peripheral);
+});
 
-// ganglion.searchStart();
+ganglion.searchStart();
 
+
+
+
+
+
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// TEST ONLY // FAKE BRAIN // TEST ONLY // FAKE BRAIN // TEST ONLY // FAKE BRAIN // TEST ONLY // FAKE BRAIN // TEST ONLY //
 // spin up the udpPort communication //
-console.log("open udpPort");
-udpPort.open();
-
-for (var i = 1; i < 1000000; i++) {
-  setTimeout(() => {
-    console.log(`index: ${i}`);
-    let sample = lib.rng();
-    console.log(sample);
-    sample.forEach((value, index) => {
-      setTimeout(() => {
-        let quad = index + 2,
-            opacity = value,
-            quadCmd = { address: `/surfaces/Quad-${quad}/select` },
-            opacityCmd = { address: `/surfaces/Quad-${quad}/opacity`, args: [{ type: "i", value: value }]};
-        udpPort.send(quadCmd, "localhost", 8010);
-        console.log("sending " + JSON.stringify(quadCmd));
-        udpPort.send(opacityCmd, "localhost", 8010);
-        console.log("sending " + JSON.stringify(opacityCmd));
-      }, 250 * index);
-    });
-  }, 1000 * i);
-}
+// console.log("open udpPort");
+// udpPort.open();
+//
+// for (var i = 1; i < 1000000; i++) {
+//   setTimeout(() => {
+//     console.log(`index: ${i}`);
+//     let sample = lib.rng();
+//     console.log(sample);
+//     sample.forEach((value, index) => {
+//       setTimeout(() => {
+//         let quad = index + 2,
+//             opacity = value,
+//             quadCmd = { address: `/surfaces/Quad-${quad}/select` },
+//             opacityCmd = { address: `/surfaces/Quad-${quad}/opacity`, args: [{ type: "i", value: value }]};
+//         udpPort.send(quadCmd, "localhost", 8010);
+//         console.log("sending " + JSON.stringify(quadCmd));
+//         udpPort.send(opacityCmd, "localhost", 8010);
+//         console.log("sending " + JSON.stringify(opacityCmd));
+//       }, 250 * index);
+//     });
+//   }, 1000 * i);
+// }
